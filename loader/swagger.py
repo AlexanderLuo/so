d2637812e788f6d7a92923da17708212de01d756
123 <321>
2017-09-15 00:54:43 +0800

    dd

---
+++ loader/swagger.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import config
+
+
+# print(os.getcwd())
+# for i in os.listdir('.'):
+#     if os.path.isdir(i):
+#         print(i)
+
+
+
+
+os.mkdir(os.getcwd()+config.SEPARATION+"swagger")
